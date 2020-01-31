d5cabc6d06d562998ce0d8bddfada1312f2ed44d
Júlio César <diolandajr@gmail.com>
2020-01-30 23:49:11 -0300

    Building form

--- app/kickhelper/views.py
+++ app/kickhelper/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from datetime import date
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the kickhelper index.")
+    template = loader.get_template('kickhelper/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def results(request):
+    main_category = request.POST['mainCategory']
+    category = request.POST['category']
+    goal = request.POST['goal']
+    country = request.POST['country']
+    currency = request.POST['currency']
+    today = date.today()
+
+    return HttpResponse(today)
